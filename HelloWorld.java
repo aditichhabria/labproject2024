d63b44fcf27518df7de035389f578e41672d3b8c
aditichhabria <121998335+aditichhabria@users.noreply.github.com>
2024-02-08 09:57:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello WOrld");
+        System.out.println("newwwwwwwww");
         System.out.println("Changes");
     }
 }
